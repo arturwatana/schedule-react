3cdf085139cb6f1c02fc9efbc835cb79b1b4280e
Artur Watanabe <arturwatanabe@gmail.com>
2023-05-14 06:10:00 -0300

    add: dateFormat

--- src/utils/DateFormat/DateFormat.tsx
+++ src/utils/DateFormat/DateFormat.tsx
@@ -1,3 +1,16 @@
-export function dateFormat(date: Date) {
-  return new Intl.DateTimeFormat("pt-BR").format(date);
+export class DateFormat {
+  constructor() {}
+
+  endDateFormat(date: string) {
+    const endDateFormated = date
+      .split("-")
+      .reverse()
+      .toString()
+      .replaceAll(",", "/");
+    return endDateFormated;
+  }
+
+  formatNewDate(date: Date) {
+    return new Intl.DateTimeFormat("pt-BR").format(date);
+  }
 }
