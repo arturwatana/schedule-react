ba137f67b552108e4792e0872aa4fadab33e9249
Artur Watanabe <arturwatanabe@gmail.com>
2023-05-14 06:11:53 -0300

    add: taskCard finished

--- src/Components/TaskCard/TaskCard.tsx
+++ src/Components/TaskCard/TaskCard.tsx
@@ -1,24 +1,62 @@
+import { Task } from "../../entities/Task/Task.entity";
 import Button from "../form/Button/Button";
 import styles from "./TaskCard.module.css";
 import { AiOutlineEdit } from "react-icons/ai";
+import { useState } from "react";
+import { TaskRepositoryFake } from "../../repositories/Tasks/fakeDB/taskRepository.fakeDB";
 
 type TaskCardProps = {
+  id: string;
   name: string;
   urgency: string;
   startDate: string;
   endDate: string;
+  completed: boolean;
   handleEditModal: React.Dispatch<React.SetStateAction<boolean>>;
+  setUpdateScreen: React.Dispatch<React.SetStateAction<boolean>>;
   isOpen: boolean;
+  editTask?: React.Dispatch<React.SetStateAction<Task>>;
 };
 
 function TaskCard({
+  id,
   name,
   urgency,
   startDate,
   endDate,
+  completed,
   handleEditModal,
+  setUpdateScreen,
   isOpen,
+  editTask,
 }: TaskCardProps) {
+  const db = new TaskRepositoryFake();
+
+  function OpenModal() {
+    if (handleEditModal && isOpen === false) {
+      if (editTask) {
+        editTask({
+          id: id,
+          name: name,
+          startDate: startDate,
+          endDate: endDate,
+          urgency: urgency,
+          completed: completed,
+        });
+      }
+      handleEditModal(true);
+    }
+  }
+
+  async function handleCompleteTask() {
+    const taskInDB = await db.findById(id);
+    if (taskInDB && taskInDB.completed === false) {
+      taskInDB.completed = true;
+      db.updateTask(taskInDB);
+      setUpdateScreen(true);
+    }
+  }
+
   return (
     <div className={styles.taskCard}>
       <p className={styles.taskCardTittle}>{name}</p>
@@ -30,15 +68,14 @@ function TaskCard({
           <span>Termino:</span> {endDate}
         </p>
         <p>
-          <span>Status:</span> {urgency}
+          <span>Urgencia:</span> {urgency}
+        </p>
+        <p>
+          <span>Status:</span> {completed ? "Concluida" : "Em andamento"}
         </p>
         <div className={styles.btn}>
-          <Button text="Concluir"></Button>
-          <Button
-            text="Editar"
-            handleEditModal={handleEditModal}
-            isOpen={isOpen}
-          >
+          <Button text="Concluir" onClick={handleCompleteTask}></Button>
+          <Button text="Editar" onClick={OpenModal}>
             <AiOutlineEdit />
           </Button>
         </div>
--- src/entities/Task/Task.entity.tsx
+++ src/entities/Task/Task.entity.tsx
@@ -1,4 +1,4 @@
-import { dateFormat } from "../../utils/DateFormat/DateFormat";
+import { DateFormat } from "../../utils/DateFormat/DateFormat";
 import { TaskProps } from "./interface/ITask";
 import { nanoid } from "nanoid";
 export class Task {
@@ -7,24 +7,21 @@ export class Task {
   urgency: string;
   startDate: string;
   endDate: string;
+  completed: boolean;
 
   private constructor({ name, urgency, endDate }: TaskProps) {
     if (!name) {
       throw new Error("Ops, toda task precisa de um nome.");
     }
-    if (!endDate) {
-      endDate = dateFormat(new Date());
-    }
-    const endDateFormated = endDate
-      .split("-")
-      .reverse()
-      .toString()
-      .replaceAll(",", "/");
+    const dateFormat = new DateFormat();
+    const endDateFormated = dateFormat.endDateFormat(endDate);
+
     this.id = nanoid();
     this.name = name;
     this.urgency = urgency ? urgency : "Pouco Urgente";
-    this.startDate = dateFormat(new Date());
+    this.startDate = dateFormat.formatNewDate(new Date());
     this.endDate = endDateFormated;
+    this.completed = false;
   }
 
   static create(props: TaskProps) {
