f5420fbf9a4da842e35569f5b713adafd83a4afd
Artur Watanabe <arturwatanabe@gmail.com>
2023-05-15 06:08:06 -0300

    add: compareDates Function

--- src/entities/Task/Task.entity.tsx
+++ src/entities/Task/Task.entity.tsx
@@ -7,21 +7,27 @@ export class Task {
   urgency: string;
   startDate: string;
   endDate: string;
-  completed: boolean;
+  completed: string;
 
   private constructor({ name, urgency, endDate }: TaskProps) {
     if (!name) {
       throw new Error("Ops, toda task precisa de um nome.");
     }
     const dateFormat = new DateFormat();
-    const endDateFormated = dateFormat.endDateFormat(endDate);
+    const endDateFormated = () => {
+      if (endDate) {
+        return dateFormat.endDateFormat(endDate);
+      }
+
+      return dateFormat.formatNewDate(new Date());
+    };
 
     this.id = nanoid();
     this.name = name;
     this.urgency = urgency ? urgency : "Pouco Urgente";
     this.startDate = dateFormat.formatNewDate(new Date());
-    this.endDate = endDateFormated;
-    this.completed = false;
+    this.endDate = endDateFormated();
+    this.completed = "Em Andamento";
   }
 
   static create(props: TaskProps) {
--- src/utils/DateFormat/DateFormat.tsx
+++ src/utils/DateFormat/DateFormat.tsx
@@ -13,4 +13,19 @@ export class DateFormat {
   formatNewDate(date: Date) {
     return new Intl.DateTimeFormat("pt-BR").format(date);
   }
+
+  compareDates(endDate: string, startDate: string) {
+    const invertDate = (date: string) => {
+      const initialDate = date.split("/");
+      const formatDate = `${initialDate[1]}/${initialDate[0]}/${initialDate[2]}`;
+      return formatDate;
+    };
+
+    const newDate = {
+      startDate: new Date(invertDate(startDate)).getTime(),
+      endDate: new Date(invertDate(endDate)).getTime(),
+    };
+
+    return newDate.startDate <= newDate.endDate;
+  }
 }
