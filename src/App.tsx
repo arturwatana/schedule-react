d4e39dbb9ae14965c724f58e7de69273b195ca5e
Artur Watanabe <arturwatanabe@gmail.com>
2023-05-21 04:36:55 -0300

    add: paths

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import "./App.css";
 import NavBar from "./Components/layout/NavBar/NavBar";
 import Footer from "./Components/layout/Footer/Footer";
 import Container from "./Components/layout/Container/Container";
-import Home from "./pages/Home/Home";
+import MyTasks from "./pages/Home/MyTasks";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NewHome from "./pages/NewHome/NewHome";
 
 function App() {
   return (
@@ -12,7 +13,9 @@ function App() {
       <NavBar />
       <Container customClass="minHeight">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<NewHome />} />
+          <Route path="/mytasks" element={<MyTasks />} />
+          <Route path="/aboutus" element={<NewHome />} />
         </Routes>
       </Container>
       <Footer />
